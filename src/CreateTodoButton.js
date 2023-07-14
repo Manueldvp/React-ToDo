208535bb9b029ff2a179d91bc0dbb95f1a367587
manueldvp <manuelbyto@gmail.com>
2023-07-14 14:57:45 -0400

    ToDo Basico

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,7 @@
+function CreateTodoButton() {
+    return(
+        <button className="CreateTodoButton" onClick={() => console.log('legiste click')}> + </button>
+    );
+  }
+
+export {CreateTodoButton};
